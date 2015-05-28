ed0f0491bc68f34a1d313b268ef8b84ab65e4df1
Bernardo Amorim <contato@bamorim.com>
2015-05-28 16:04:11 -0300

    Savon 2.11 works just fine

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'savon', '~> 2.3'
+gem 'savon', '~> 2.11'
 gem 'nori'
 gem 'gyoku'
 
--- tests/test_configuration.rb
+++ tests/test_configuration.rb
@@ -1,5 +1,5 @@
 module TestConfiguration
   module Merchant
-    MerchantKey = ''
+    MerchantKey = '871E91A3-55F0-4E95-958D-9504E0D2FE0D'
   end
 end
